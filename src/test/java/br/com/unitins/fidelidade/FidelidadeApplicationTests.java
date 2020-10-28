4acea052ec9ab32836e572034cc123def1e21d5b
Iury Felipe <iurycomputacao@gmail.com>
2020-10-27 22:03:26 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/unitins/fidelidade/FidelidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unitins.fidelidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
