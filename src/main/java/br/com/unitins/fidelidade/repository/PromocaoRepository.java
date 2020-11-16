d97f84d4c4f4eb7768356131aabc0a9c6acbd33c
Tales <talesmelquiades@hotmail.com>
2020-11-16 09:28:28 -0300

    merge

--- src/main/java/br/com/unitins/fidelidade/repository/PromocaoRepository.java
+++ src/main/java/br/com/unitins/fidelidade/repository/PromocaoRepository.java
@@ -7,16 +7,9 @@ import org.springframework.data.repository.query.Param;
 import br.com.unitins.fidelidade.model.Promocao;
 
 public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
-<<<<<<< HEAD
-
-	Promocao findById(long id);
-	Promocao findByNome(String nome);
-}
-=======
 	
 	@Query("SELECT p FROM Promocao p WHERE p.status = 'true' AND p.idPromocao = :id")
 	Promocao findById(@Param("id") long id);
 	@Query("SELECT p FROM Promocao p WHERE p.status = 'true' AND p.nome like :nome")
 	Promocao findByNome(@Param("nome") String nome);
 }
->>>>>>> 16d574e6740a21c3c8a4d2dfba8c843dbd8e0643
