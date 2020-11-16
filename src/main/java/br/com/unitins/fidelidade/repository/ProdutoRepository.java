0d97074979fb91be2fc453b0aea61d5f88c06599
gustavo <gustavo@gustavo-Ubuntu>
2020-11-16 13:48:56 -0300

    Resource de Login adicionado.

--- src/main/java/br/com/unitins/fidelidade/repository/ProdutoRepository.java
+++ src/main/java/br/com/unitins/fidelidade/repository/ProdutoRepository.java
@@ -1,11 +1,16 @@
 package br.com.unitins.fidelidade.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.unitins.fidelidade.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
 	Produto findById(long id);
-	Produto findByNome(String nome);
+	List<Produto> findByNome(String nome);
+	@Query(value = "SELECT p FROM Produto p WHERE p.status = 'true'")
+	List<Produto> findAllAtivos();
 }
---
+++ src/main/java/br/com/unitins/fidelidade/resource/LoginResource.java
@@ -0,0 +1,50 @@
+package br.com.unitins.fidelidade.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.unitins.fidelidade.model.Cliente;
+import br.com.unitins.fidelidade.model.Funcionario;
+import br.com.unitins.fidelidade.repository.ClienteRepository;
+import br.com.unitins.fidelidade.repository.FuncionarioRepository;
+
+@RestController
+@RequestMapping(value = "/fidelidade")
+public class LoginResource {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	@Autowired
+	FuncionarioRepository funcionarioRepository;
+	
+	@GetMapping("/login/cliente/{email}/{telefone}")
+	public String autenticarCliente(@PathVariable(value = "email") String email, 
+			@PathVariable(value= "telefone") String telefone) {
+		Cliente cliente = clienteRepository.findByEmail(email);
+		
+		if(cliente == null)
+			return "Email ou senha inválidos.";
+		if(!cliente.getTelefone().equals(telefone))
+			return "Email ou senha inválidos.";
+		
+		return "Acesso liberado.";
+		
+	}
+	
+	@GetMapping("/login/funcionario/{email}/{senha}")
+	public String autenticarFuncionario(@PathVariable(value = "email") String email, 
+			@PathVariable(value= "senha") String senha) {
+		Funcionario funcionario = funcionarioRepository.findByEmail(email);
+		
+		if(funcionario == null)
+			return "Email ou senha inválidos.";
+		if(!funcionario.getSenha().equals(senha))
+			return "Email ou senha inválidos.";
+		
+		return "Acesso liberado.";
+		
+	}
+}
